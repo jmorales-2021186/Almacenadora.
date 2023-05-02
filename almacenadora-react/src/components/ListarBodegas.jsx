926547411674dde3a5561c7fc23afd063d1c272d
mhurtado2021407 <mhurtado-2021407@kinal.edu.gt>
2023-05-01 19:54:33 -0600

    se agrego la disponibilidad en bodegas mhurtado

--- almacenadora-react/src/components/ListarBodegas.jsx
+++ almacenadora-react/src/components/ListarBodegas.jsx
@@ -1,54 +1,57 @@
-import React, { useContext } from 'react'
-import {Link} from 'react-router-dom'
-import { NombreContexto } from '../Index'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { NombreContexto } from "../Index";
 
-export const ListarBodegas = ({ id, name, description, location, size, price, deleteBodega }) => {
-    
-    const { dataUser } = useContext(NombreContexto)
+export const ListarBodegas = ({
+  id,
+  name,
+  description,
+  location,
+  size,
+  price,
+  availability,
+  deleteBodega,
+}) => {
+  const { dataUser } = useContext(NombreContexto);
 
-    
-    
-    return (
-        <>
-        
-             <div className="card m-5">
-                <div className="card-header">
-                    bodega
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">Descripcion: {description}</p>
-                    <p className="card-text">Tamaño: {size}</p>
-                    <p className="card-text">Precio: {price}</p>
-                    <p className="card-text">Locacion: {location}</p>
+  return (
+    <>
+      <div className="card m-5">
+        <div className="card-header">bodega</div>
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">
+            Disponibilidad: {availability ? "Disponible" : "NO DISPONIBLE"}
+          </p>
+          <p className="card-text">Descripcion: {description}</p>
+          <p className="card-text">Tamaño: {size}</p>
+          <p className="card-text">Precio: {price}</p>
+          <p className="card-text">Locacion: {location}</p>
 
-                    {
-                        dataUser.role === 'TRABAJADOR' ? (
+          {dataUser.role === "TRABAJADOR" ? (
+            <Link className="btn btn-primary">Reservar</Link>
+          ) : (
+            <Link className="btn btn-success ms-1">Go somewhere</Link>
+          )}
 
-                            <Link   className="btn btn-primary">Reservar</Link>
-                        ): (
-                            <Link  className="btn btn-success ms-1">Go somewhere</Link>
-                        )
-
-                    }
-
-                    {
-                        dataUser.role === 'ADMIN'
-                        ?(
-                            <>
-                                <Link to={`/updateB/${id}`}  className="btn btn-warning ms-1">Editar</Link>
-                                <Link onClick={()=> deleteBodega(id)}  className="btn btn-danger ms-1">Eliminar</Link>
-                            </>
-                         ):<></>
-                    }
-
-
-
-                </div>
-                <div className="card-footer text-muted">
-                    2 days ago
-                </div>
-            </div> 
-        </>
-    )
-}
+          {dataUser.role === "ADMIN" ? (
+            <>
+              <Link to={`/updateB/${id}`} className="btn btn-warning ms-1">
+                Editar
+              </Link>
+              <Link
+                onClick={() => deleteBodega(id)}
+                className="btn btn-danger ms-1"
+              >
+                Eliminar
+              </Link>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className="card-footer text-muted">2 days ago</div>
+      </div>
+    </>
+  );
+};
--- almacenadora-react/src/pages/BodegasPage/BodegasPage.jsx
+++ almacenadora-react/src/pages/BodegasPage/BodegasPage.jsx
@@ -1,106 +1,120 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { NavBar } from '../../components/NavBar'
-import { ListarBodegas } from '../../components/ListarBodegas'
-import axios from 'axios'
-import { NombreContexto } from '../../Index'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect, useContext } from "react";
+import { NavBar } from "../../components/NavBar";
+import { ListarBodegas } from "../../components/ListarBodegas";
+import axios from "axios";
+import { NombreContexto } from "../../Index";
+import { Link } from "react-router-dom";
 
 export const BodegasPage = () => {
+  const { disponible, setDisponible, dataUser } = useContext(NombreContexto);
+  const [bodegas, setBodegas] = useState([]);
+  const [alamacenador, setAlmacenador] = useState({});
+  const [busqueda, setBusqueda] = useState("");
 
-    const { disponible, setDisponible, dataUser } = useContext(NombreContexto)
-    const [bodegas, setBodegas] = useState([]);
-    const [alamacenador, setAlmacenador] = useState({})
-    const [busqueda, setBusqueda]= useState('')
-
-    const getBodegas = async () => {
-        try {
-            const { data } = await axios('http://localhost:3418/storage/get');
-            setBodegas(data.storages)
-            setAlmacenador(data.storages)
-        } catch (e) {
-            console.log(e);
-        }
+  const getBodegas = async () => {
+    try {
+      const { data } = await axios("http://localhost:3418/storage/get");
+      setBodegas(data.storages);
+      setAlmacenador(data.storages);
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    useEffect(() => getBodegas, [])
+  useEffect(() => getBodegas, []);
 
-    const handleChange = (e) => {
-        setBusqueda(e.target.value)
-        filtrar(e.target.value)
+  const handleChange = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  };
+
+  //Busqueda
+  const filtrar = (terminoDeBusqueda) => {
+    var resultados = alamacenador.filter((element) => {
+      if (
+        element.name
+          .toString()
+          .toLowerCase()
+          .includes(terminoDeBusqueda.toLowerCase())
+      ) {
+        return element;
+      }
+    });
+    setBodegas(resultados);
+  };
+
+  //Eliminar bodegas
+  const deleteBodega = async (id) => {
+    try {
+      let mensaje = confirm("Seguro de que quiere eliminar la bodega");
+      if (mensaje) {
+        const { data } = await axios.delete(
+          `http://localhost:3418/storage/delete/${id}`
+        );
+        getBodegas();
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    //Busqueda
-    const filtrar = (terminoDeBusqueda)=>{
-        var resultados = alamacenador.filter((element)=>{
-            if(element.name.toString().toLowerCase().includes(terminoDeBusqueda.toLowerCase())){
-                return element;
-            }
-        })
-        setBodegas(resultados);
-    }
+  return (
+    <>
+      <NavBar />
 
-    //Eliminar bodegas
-    const deleteBodega = async (id) =>{
-        try{
-            let mensaje = confirm('Seguro de que quiere eliminar la bodega');
-            if(mensaje){
-                const { data } = await axios.delete(`http://localhost:3418/storage/delete/${id}`);
-                getBodegas()
-            }
-        }catch(e){  
-            console.log(e);
-        }
-    }
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-    return (
-        <>
+      <div className="contenedor">
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Buscar"
+            aria-label="Buscar"
+            value={busqueda}
+            onChange={handleChange}
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Buscar
+          </button>
+        </form>
+      </div>
 
-            <NavBar />
+      <div className="contenedor">
+        {dataUser.role === "ADMIN" ? (
+          <Link to="/addBodega" className="btn btn-primary mt-5 ms-5">
+            Nueva Bodega
+          </Link>
+        ) : (
+          <></>
+        )}
 
-            <br /><br /><br /><br /><br />
-
-
-
-
-            <div className='contenedor'>
-
-                <form className="d-flex" role="search">
-                    <input
-                        className="form-control me-2"
-                        type="search"
-                        placeholder="Buscar"
-                        aria-label="Buscar"
-                        value={busqueda}
-                        onChange={handleChange}
-                    />
-                    <button className="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-            </div>
-
-            <div className='contenedor'>
-                {
-                    dataUser.role === 'ADMIN'
-                        ? <Link to='/addBodega' className="btn btn-primary mt-5 ms-5">Nueva Bodega</Link>
-                        : <></>
-                }
-
-                {
-                    bodegas.map(({ _id, name, description, location, size, price }, i) => {
-                        return (
-                            <ListarBodegas 
-                                key={i}
-                               id={_id} 
-                               name={name} 
-                               description={description} 
-                               location={location}
-                                size={size} 
-                                price={price} 
-                                deleteBodega={deleteBodega}/>
-                        )
-                    })
-                }
-            </div>
-
-        </>
-    )
-}
+        {bodegas.map(
+          (
+            { _id, name, description, location, size, price, availability },
+            i
+          ) => {
+            console.log(bodegas[0].availability);
+            return (
+              <ListarBodegas
+                availability={availability}
+                key={i}
+                id={_id}
+                name={name}
+                description={description}
+                location={location}
+                size={size}
+                price={price}
+                deleteBodega={deleteBodega}
+              />
+            );
+          }
+        )}
+      </div>
+    </>
+  );
+};
