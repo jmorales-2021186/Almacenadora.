56bfd7325589df3576b84c17c7663eb814fd4123
mhurtado2021407 <mhurtado-2021407@kinal.edu.gt>
2023-05-01 18:53:46 -0600

    se agregaron funcionalidades a agregar trabajador y se corregieron rutas en el back en usuarios

--- almacenadora-react/src/components/NavBar.jsx
+++ almacenadora-react/src/components/NavBar.jsx
@@ -1,58 +1,54 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { NombreContexto } from '../Index'
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { NombreContexto } from "../Index";
 
 export const NavBar = () => {
+  const { loggedIn, setLoggedIn, dataUser, setDataUser } =
+    useContext(NombreContexto);
 
-    const { loggedIn, setLoggedIn, dataUser, setDataUser } = useContext(NombreContexto)
+  return (
+    <>
+      <div className="navDiv">
+        <header>
+          <div className="contenedor">
+            <Link to="/">
+              <h1>
+                Almace<span className="verde">nadora</span>
+              </h1>
+            </Link>
+            <nav>
+              <Link to="/bodegas">🏬Bodegas</Link>
 
-    return (
-        <>
-            <header>
-                <div className="contenedor">
-                    <h1>Almace<span className='verde'>nadora</span></h1>
-                    <nav>
-                        <Link to='/bodegas'>
-                            🏬Bodegas
-                        </Link>
+              <Link>Servicios</Link>
+              <Link>Servicios Adicionales</Link>
 
-                        <Link>
-                            Servicios
-                        </Link>
-                        <Link>
-                            Servicios Adicionales
-                        </Link>
-
-{/* 
+              {/* 
                         <Link>👤{dataUser.role}</Link> */}
 
-                        {
-                            dataUser.role === 'ADMIN'
-                                ? <Link >Nuevo Usuario</Link>
-                                : <></>
-                        }
+              {dataUser.role === "ADMIN" ? (
+                <Link to="/addUser">Nuevo Usuario</Link>
+              ) : (
+                <></>
+              )}
 
-                        {
-                            loggedIn ? (
-                                <Link to='/' onClick={() => {
-                                    localStorage.clear()
-                                    setDataUser({})
-                                    setLoggedIn(false)
-                                }}>
-                                    Cerrar Secion
-                                </Link>
-                            ) : (
-                                <Link to='/login'>
-                                    Iniciar Secion
-                                </Link>
-
-                            )
-                        }
-
-                    </nav>
-                </div>
-            </header>
-        </>
-    )
-}
+              {loggedIn ? (
+                <Link
+                  to="/"
+                  onClick={() => {
+                    localStorage.clear();
+                    setDataUser({});
+                    setLoggedIn(false);
+                  }}
+                >
+                  Cerrar Secion
+                </Link>
+              ) : (
+                <Link to="/login">Iniciar Secion</Link>
+              )}
+            </nav>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
--- almacenadora-react/src/pages/NewUser/AddUser.jsx
+++ almacenadora-react/src/pages/NewUser/AddUser.jsx
@@ -1,40 +1,133 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavBar } from "../../components/NavBar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const AddUser = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    surname: "",
+    username: "",
+    password: "",
+    email: "",
+    phone: "",
+    role: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const register = async (e) => {
+    try {
+      e.preventDefault();
+      const { data } = await axios.post(
+        "http://localhost:3418/registerAdmin",
+        form
+      );
+      if (data.message) {
+        alert(data.message);
+        navigate("/login");
+      }
+    } catch (err) {
+      console.log(err);
+      alert(err.response.data.message);
+      throw new Error("Error registering user");
+    }
+  };
   return (
-    <> 
-    <div className='container'>
-        <h3 className='text-center'>Sing Up</h3>
-        <form className='m-5 text-center'>
-            <div className='mb-3'>
-                <label className='form-label' htmlFor="">Name</label>
-                <input onChange={handleChange} name='name' className='form-control' type="text" />
-            </div>
-            <div className='mb-3'>
-                <label className='form-label' htmlFor="">Surname</label>
-                <input onChange={handleChange} name='surname' className='form-control' type="text" />
-            </div>
-            <div className='mb-3'>
-                <label className='form-label' htmlFor="">Username</label>
-                <input onChange={handleChange} name='username' className='form-control' type="text" />
-            </div>
-            <div className='mb-3'>
-                <label className='form-label' htmlFor="">Password</label>
-                <input onChange={handleChange} name='password' className='form-control' type="text" />
-            </div>
-            <div className='mb-3'>
-                <label className='form-label' htmlFor="">Email</label>
-                <input onChange={handleChange} name='email' className='form-control' type="text" />
-            </div>
-            <div className='mb-3'>
-                <label className='form-label' htmlFor="">Phone</label>
-                <input onChange={handleChange} name='phone' className='form-control' type="text" />
-            </div>
-            <button onClick={(e)=> register(e)} className='btn btn-primary'>
-                Sign Up
-            </button>
+    <>
+      <NavBar />
+      <div className="container ">
+        <h3 className="text-center">Sing Up</h3>
+        <form className="m-5 text-center">
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Name
+            </label>
+            <input
+              onChange={handleChange}
+              name="name"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Surname
+            </label>
+            <input
+              onChange={handleChange}
+              name="surname"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Username
+            </label>
+            <input
+              onChange={handleChange}
+              name="username"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Password
+            </label>
+            <input
+              onChange={handleChange}
+              name="password"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Email
+            </label>
+            <input
+              onChange={handleChange}
+              name="email"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Phone
+            </label>
+            <input
+              onChange={handleChange}
+              name="phone"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="">
+              Role
+            </label>
+            <input
+              onChange={handleChange}
+              name="role"
+              className="form-control"
+              type="text"
+            />
+          </div>
+          <button onClick={(e) => register(e)} className="btn btn-primary">
+            Sign Up
+          </button>
         </form>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
